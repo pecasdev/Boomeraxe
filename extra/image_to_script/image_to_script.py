b119c2a5d3b97e681c56b4c1cb2a5ac5bae492a9
pecancake <33271004+pecancake@users.noreply.github.com>
2017-12-03 13:45:12 -0500

    Extra
    Added folder with demos and image_to_script.

---
+++ extra/image_to_script/image_to_script.py
@@ -0,0 +1,114 @@
+from PIL import Image
+import os.path
+import os
+import time
+
+os.system('mode con: cols=140 lines=40')
+
+def read_pixel(x,y,pixel):
+    global script
+    global corners
+    
+    obj='empty'
+    
+    if pixel==(000,000,000):    obj='obj_wall'
+    if pixel==(000,000,100):    obj='obj_wall_crumble'
+    if pixel==(000,000,200):    obj='obj_platform'
+    if pixel==(100,000,000):    obj='obj_spike'
+    if pixel==(200,000,000):    obj='obj_spike_retract'
+    if pixel==(000,200,000):    obj='obj_savepoint'
+    if pixel==(200,200,000):    obj='obj_torch'
+    if pixel==(250,000,000):    obj='obj_enemy0'
+    if pixel==(250,000,050):    obj='obj_enemy1'
+    if pixel==(250,000,100):    obj='obj_enemy2'
+    if pixel==(250,000,150):    obj='obj_enemy3'
+
+    if obj!='empty':
+        script[x+y*img.size[0]]='instance_create({},{},{})'.format(x*20,y*20,obj)
+
+    else:
+        script[x+y*img.size[0]]='//({},{},empty)'.format(x*20,y*20)
+        
+    return '[ {} ]'.format(obj).ljust(20)+'[ {} , {} ]'.format(x*20,y*20).ljust(20)+'[ {} / {} ]'.format(x+1+y*(corners[3]-corners[1]),(corners[3]-corners[1])*(corners[2]-corners[0])).ljust(20)+'[ #{} ]'.format(x+1+y*img.size[0]).ljust(20)+'[ {}% ]'.format(round((1.0*x+1+y*(corners[3]-corners[1]))/((corners[3]-corners[1])*(corners[2]-corners[0]))*100,2)).ljust(20)
+
+def convert_image():
+    global corners
+    global img
+    global script
+    
+    for y in range(corners[1],corners[3]):
+        for x in range(corners[0],corners[2]):
+            pixel=pix[x,y]
+            
+            print read_pixel(x,y,pixel)
+
+    print '\n\nWriting script...  ',
+    with open('boomeraxe_room.txt','w') as f:
+        for item in script:
+            f.write(item.strip('\n')+'\n')
+            
+    print 'DONE'
+    
+print '-----[ IMAGE >> SCRIPT v2.0 ]-----'
+
+if os.path.isfile('boomeraxe_room.png') and os.path.isfile('boomeraxe_room.txt'):
+    img=Image.open('boomeraxe_room.png')
+    pix=img.load()
+    
+elif os.path.isfile('E:\image_to_script\boomeraxe.png') and os.path.isfile('E:\image_to_script\boomeraxe.txt'):
+    img=Image.open('E:\image_to_script\boomeraxe.png')
+    pix=img.load()
+
+else:
+    print '\nFile(s) Missing : Make sure \'boomeraxe_room.PNG\' and \'boomeraxe_room.TXT\' are in the same folder as \'image_to_script.PY\''
+    print '\nPress any key to exit...'
+    raw_input()
+    quit()
+
+while True:
+    print '\n'    
+    print '[0] Entire image convert'
+    print '[1] Section convert'
+    print '\n'
+    
+    choice=raw_input()
+    script=['0']*(img.size[0]*img.size[1])
+    
+    if choice=='0':
+        corners=[0,0,img.size[0],img.size[1]]   # x_min, y_min, x_max, y_max
+        convert_image()
+        break
+        
+    elif choice=='1':        
+        while True:
+            try:
+                print 'Input pixel corners (x_min,y_min,x_max,y_max) : '
+                c_string=raw_input()
+
+                corners=c_string.split(',')
+                corners=map(int,corners)
+                             
+                # Keeping within image bounds
+                corners[0]=max(corners[0],0)
+                corners[1]=max(corners[1],0)
+                corners[2]=min(corners[2],img.size[0])
+                corners[3]=min(corners[3],img.size[1])
+                
+                break
+            
+            except:
+                print '\nError in input\n\n'
+        
+        with open('boomeraxe_room.txt','r+') as f:
+            prev_data=f.readlines()
+            
+            if len(prev_data)==img.size[0]*img.size[1]:
+                script=prev_data
+                  
+        convert_image()
+        break
+    
+    else:
+        print 'Invalid choice'
+
+raw_input('\nArigato! >.< (Enter to exit)')
